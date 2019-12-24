2bd15ffd1603b84d90b5b0dde91387d03e4ad1eb
Erick Simões <erick.simoes@live.com>
2019-12-23 21:40:32 -0300

    Translate variables names

--- carleto/main.py
+++ carleto/main.py
@@ -2,17 +2,17 @@
 
 from carleto.student.student import TemplateStudent, Competence
 
-facilitador = TemplateStudent("facilitador", {Competence.ORGANIZATION, Competence.EMPATHY, Competence.COMMUNICATION})
-facilitador.set_competences(3, 5, 4, 1, 2)
+facilitator = TemplateStudent("facilitador", {Competence.ORGANIZATION, Competence.EMPATHY, Competence.COMMUNICATION})
+facilitator.set_competences(3, 5, 4, 1, 2)
 
-pesquisador = TemplateStudent("pesquisador", {Competence.CURIOSITY, Competence.ORGANIZATION, Competence.COMMUNICATION})
-pesquisador.set_competences(3, 4, 1, 5, 2)
+researcher = TemplateStudent("pesquisador", {Competence.CURIOSITY, Competence.ORGANIZATION, Competence.COMMUNICATION})
+researcher.set_competences(3, 4, 1, 5, 2)
 
-analista = TemplateStudent("analista", {Competence.INTERPRETATION, Competence.COMMUNICATION, Competence.ORGANIZATION})
-analista.set_competences(4, 3, 1, 2, 5)
+analyst = TemplateStudent("analista", {Competence.INTERPRETATION, Competence.COMMUNICATION, Competence.ORGANIZATION})
+analyst.set_competences(4, 3, 1, 2, 5)
 
-revisor = TemplateStudent("revisor", {Competence.CURIOSITY, Competence.INTERPRETATION, Competence.COMMUNICATION})
-revisor.set_competences(3, 1, 2, 5, 4)
+reviser = TemplateStudent("revisor", {Competence.CURIOSITY, Competence.INTERPRETATION, Competence.COMMUNICATION})
+reviser.set_competences(3, 1, 2, 5, 4)
 
-suporte = TemplateStudent("suporte", {Competence.COMMUNICATION, Competence.EMPATHY, Competence.INTERPRETATION})
-suporte.set_competences(5, 2, 4, 1, 3)
+support = TemplateStudent("suporte", {Competence.COMMUNICATION, Competence.EMPATHY, Competence.INTERPRETATION})
+support.set_competences(5, 2, 4, 1, 3)
