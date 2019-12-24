a8024e1712ff4422272c80b0671920bee2a759e3
Erick Simões <erick.simoes@live.com>
2019-12-23 21:45:50 -0300

    Fiz typo in name of template objects

--- carleto/main.py
+++ carleto/main.py
@@ -2,17 +2,17 @@
 
 from carleto.student.student import TemplateStudent, Competence
 
-facilitator = TemplateStudent("facilitador", {Competence.ORGANIZATION, Competence.EMPATHY, Competence.COMMUNICATION})
+facilitator = TemplateStudent("facilitator", {Competence.ORGANIZATION, Competence.EMPATHY, Competence.COMMUNICATION})
 facilitator.set_competences(3, 5, 4, 1, 2)
 
-researcher = TemplateStudent("pesquisador", {Competence.CURIOSITY, Competence.ORGANIZATION, Competence.COMMUNICATION})
+researcher = TemplateStudent("researcher", {Competence.CURIOSITY, Competence.ORGANIZATION, Competence.COMMUNICATION})
 researcher.set_competences(3, 4, 1, 5, 2)
 
-analyst = TemplateStudent("analista", {Competence.INTERPRETATION, Competence.COMMUNICATION, Competence.ORGANIZATION})
+analyst = TemplateStudent("analyst", {Competence.INTERPRETATION, Competence.COMMUNICATION, Competence.ORGANIZATION})
 analyst.set_competences(4, 3, 1, 2, 5)
 
-reviser = TemplateStudent("revisor", {Competence.CURIOSITY, Competence.INTERPRETATION, Competence.COMMUNICATION})
+reviser = TemplateStudent("reviser", {Competence.CURIOSITY, Competence.INTERPRETATION, Competence.COMMUNICATION})
 reviser.set_competences(3, 1, 2, 5, 4)
 
-support = TemplateStudent("suporte", {Competence.COMMUNICATION, Competence.EMPATHY, Competence.INTERPRETATION})
+support = TemplateStudent("support", {Competence.COMMUNICATION, Competence.EMPATHY, Competence.INTERPRETATION})
 support.set_competences(5, 2, 4, 1, 3)
