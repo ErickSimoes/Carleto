1f1bb541975c48d2246cc1b6d8e4f0ce59c94de5
Erick Simões <erick.simoes@live.com>
2019-12-26 01:32:31 -0300

    Define method to create students by csv file

--- carleto/csv_read/build_student.py
+++ carleto/csv_read/build_student.py
@@ -1,29 +1,26 @@
 # -*- coding: utf-8 -*-
 
 import csv
+from carleto.student.student import Student
 
-from carleto.student.student import Student, Competence
 
-students = []
+def build_student_from_csv(csv_source: str) -> list:
+    students = []
 
-with open("test_file.csv", encoding="utf-8") as csv_file:
-    reader = csv.DictReader(csv_file)
+    with open(csv_source, encoding="utf-8") as csv_file:
+        reader = csv.DictReader(csv_file)
 
-    for row in reader:
-        s = Student(row["Nome"])
-        s.set_class(row["Turma"])
+        for row in reader:
+            student = Student(row["Nome"])
+            student.set_class(row["Turma"])
 
-        communication = row["Quais são suas habilidades mais fortes? [Comunicação]"]
-        organization = row["Quais são suas habilidades mais fortes? [Organização]"]
-        empathy = row["Quais são suas habilidades mais fortes? [Empatia]"]
-        curiosity = row["Quais são suas habilidades mais fortes? [Curiosidade]"]
-        interpretation = row["Quais são suas habilidades mais fortes? [Interpretação]"]
+            communication = row["Quais são suas habilidades mais fortes? [Comunicação]"]
+            organization = row["Quais são suas habilidades mais fortes? [Organização]"]
+            empathy = row["Quais são suas habilidades mais fortes? [Empatia]"]
+            curiosity = row["Quais são suas habilidades mais fortes? [Curiosidade]"]
+            interpretation = row["Quais são suas habilidades mais fortes? [Interpretação]"]
 
-        s.set_competences(communication, organization, empathy, curiosity, interpretation)
+            student.set_competences(communication, organization, empathy, curiosity, interpretation)
 
-        students.append(s)
-
-for s in students:
-    print(s.name)
-    print(s.competences[Competence.COMMUNICATION])
-    print(s.room_class)
+            students.append(student)
+    return students
